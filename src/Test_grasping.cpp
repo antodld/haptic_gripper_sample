04a25b5109710d1412cbd503f8f6231102aa666e
adallard <antonin.dallard@lirmm.fr>
2022-08-19 15:01:58 +0900

    first commit

--- src/Test_grasping.cpp
+++ src/Test_grasping.cpp
@@ -9,17 +9,73 @@ Test_grasping::Test_grasping(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rt
   solver().addTask(postureTask);
   solver().setContacts({{}});
 
+  if (config.has("gripper"))
+  {
+    config("gripper",gripper_);
+  }
+  if (config.has("sub_topic"))
+  {
+    config("sub_topic",hand_topic_);
+  }
+  if (config.has("pub_topic"))
+  {
+    config("pub_topic",hand_pub_topic);
+  }
+
+
+  nh_ = mc_rtc::ROSBridge::get_node_handle();
+  spinThread_ = std::thread(std::bind(&Test_grasping::rosSpinner, this));
+
+  logger().addLogEntry("Gripper_openning_command", [this]() -> const double & {return openning_val_;});
+
+  mc_rtc::log::info("Subscribing to {}",hand_topic_);
+
+
+  hand_sub_.subscribe(*nh_, hand_topic_);
+  hand_sub_.maxTime(maxTime_);
+  gripper_pub_ = nh_->advertise<std_msgs::Int64>(hand_pub_topic, 1);
+  controller_dt_ = dt;
   mc_rtc::log::success("Test_grasping init done ");
 }
 
 bool Test_grasping::run()
 {
+  
+  auto & gripper = robot().gripper(gripper_);
+  hand_sub_.tick(controller_dt_);
+  
+  if (hand_sub_.data().isValid()){
+    
+    openning_val_ = hand_sub_.data().value();
+
+  }
+  gripper.setTargetOpening(openning_val_);
+
+  //Calcul
+
+  //Calcul
+
+  std_msgs::Int64 pub_msg;
+  pub_msg.data = 0;
+  gripper_pub_.publish(pub_msg);
+
   return mc_control::MCController::run();
 }
 
 void Test_grasping::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
+}
+void Test_grasping::rosSpinner()
+{
+  mc_rtc::log::info("ROS spinner thread created");
+  ros::Rate r(freq_);
+  while(ros::ok())
+  {
+    ros::spinOnce();
+    r.sleep();
+  }
+  mc_rtc::log::info("ROS spinner destroyed");
 }
 
 CONTROLLER_CONSTRUCTOR("Test_grasping", Test_grasping)
--- src/Test_grasping.h
+++ src/Test_grasping.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <mc_control/mc_controller.h>
-
+#include <std_msgs/Int64.h>
+#include "ROSSubscriber.h"
 
 #include "api.h"
 
@@ -13,5 +14,18 @@ struct Test_grasping_DLLAPI Test_grasping : public mc_control::MCController
 
     void reset(const mc_control::ControllerResetData & reset_data) override;
 private:
+    
+    void rosSpinner();
+    double controller_dt_;
+    double openning_val_ = 1;
+    std::string gripper_;
     mc_rtc::Configuration config_;
+    std::shared_ptr<ros::NodeHandle> nh_;
+    std::thread spinThread_;
+    double maxTime_ = 0.5;
+    double freq_ = 0.01;
+    std::string hand_topic_ = "topic";
+    std::string hand_pub_topic = "";
+    ROSFloatSubscriber hand_sub_;
+    ros::Publisher gripper_pub_;
 };
